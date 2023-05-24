f6bc6aeecbf550e65351f9a4c47a16a5dc369c83
dakshk11 <97373190+dakshk11@users.noreply.github.com>
2023-05-24 15:06:56 -0700

    sf

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var games = ["Slither", "2048", "bballegends", "commandstrikefps", "CrketCty", "crossyroad", "drivemad", "edgesurf", "ev", "flappybird", "fortnitez", "fruitninja", "geodash", "gettingoverit", "headsoccer", "holeio", "motox3m", "pacman", "paperio", "papermc", "Pokemonscratch", "pokemonshowdown", "retrobowl", "shellshockers", "sldratk2", "sldratk3", "smashkartsio", "sonicthehedgehog", "supermariobros", "tag", "tetris", "timbercraft", "tunnelrsh"]
+const games = ["Slither", "2048", "bballegends", "commandstrikefps", "CrketCty", "crossyroad", "drivemad", "edgesurf", "ev", "flappybird", "fortnitez", "fruitninja", "geodash", "gettingoverit", "headsoccer", "holeio", "motox3m", "pacman", "paperio", "papermc", "Pokemonscratch", "pokemonshowdown", "retrobowl", "shellshockers", "sldratk2", "sldratk3", "smashkartsio", "sonicthehedgehog", "supermariobros", "tag", "tetris", "timbercraft", "tunnelrsh"]
 function random_item(games)
 {
   
@@ -8,5 +8,5 @@ return games[Math.floor(Math.random()*games.length)];
 
 var randGam = random_item(games)
 
-a = document.getElementById(randGame);
+a = document.getElementById("randGame");
 a.setAttribute("href", `Games/${randGam}.html`);
