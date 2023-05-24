e2f6a6e6454a158aead20bfb3903c9ed8fa9a8d4
dakshk11 <97373190+dakshk11@users.noreply.github.com>
2023-05-23 18:32:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var games = [Slither, 2048, bballegends, commandstrikefps, CrketCty, crossyroad, drivemad, edgesurf, ev, flappybird, fortnitez, fruitninja, geodash, gettingoverit, headsoccer, holeio, motox3m, pacman, paperio, papermc, Pokemonscratch, pokemonshowdown, retrobowl, shellshockers, sldratk2, sldratk3, smashkartsio, sonicthehedgehog, supermariobros, tag, tetris, timbercraft, tunnelrsh]
+var games = ["Slither", "2048", "bballegends", "commandstrikefps", "CrketCty", "crossyroad", "drivemad", "edgesurf", "ev", "flappybird", "fortnitez", "fruitninja", "geodash", "gettingoverit", "headsoccer", "holeio", "motox3m", "pacman", "paperio", "papermc", "Pokemonscratch", "pokemonshowdown", "retrobowl", "shellshockers", "sldratk2", "sldratk3", "smashkartsio", "sonicthehedgehog", "supermariobros", "tag", "tetris", "timbercraft", "tunnelrsh"]
 function random_item(games)
 {
   
@@ -6,4 +6,7 @@ return games[Math.floor(Math.random()*games.length)];
      
 }
 
-var randGame = random_item(games)
+var randGam = random_item(games)
+
+a = document.getElementById(randGame);
+a.setAttribute("href", `Games/${randGam}.html`);
